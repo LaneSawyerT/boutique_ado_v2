8fd266943c46ce9169a7b67149b133850b644b0d
LaneSawyerT <Lanefair@gmail.com>
2022-03-12 14:02:37 +0000

    deployed again

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('profile/', include('profiles.urls')),
     path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
